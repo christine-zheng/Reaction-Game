5f266b2ca44fbd80fbc8d19cc2efcb995f4d9279
Christine Zheng <zchristine11@gmail.com>
2022-11-02 23:18:02 -0400

    update Home component to display instructions or <Game />

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import Game from './Game';
 
 /**
  * COMPONENT
  */
 export const Home = (props) => {
+  const [game, setGame] = useState(false);
   const { username } = props;
 
-  return (
-    <div>
-      <h3>Welcome, {username}</h3>
-    </div>
-  );
+  function startGame() {
+    setGame(true);
+  }
+
+  if (!game) {
+    return (
+      <main>
+        <h1>Welcome, {username}</h1>
+        <p>Play to see your reaction time!</p>
+        <p>Click on the box when you see the color changes.</p>
+        <button type="button" onClick={setGame}>
+          Start Game!
+        </button>
+      </main>
+    );
+  }
+  return <Game />;
 };
 
 /**
